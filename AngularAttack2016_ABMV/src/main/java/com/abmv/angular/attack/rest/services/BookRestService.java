170c197ae33b74ca09dd2c4614f9096bc75413ac
Vikash Kaushik <vikash.kaushik@accenture.com>
2016-05-15 00:11:02 +0530

    rest changes

--- AngularAttack2016_ABMV/src/main/java/com/abmv/angular/attack/rest/services/BookRestService.java
+++ AngularAttack2016_ABMV/src/main/java/com/abmv/angular/attack/rest/services/BookRestService.java
@@ -1,20 +1,19 @@
 package com.abmv.angular.attack.rest.services;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abmv.angular.attack.entities.es.BookES;
 import com.abmv.angular.attack.entities.sql.Book;
-import com.abmv.angular.attack.entities.sql.Genre;
 import com.abmv.angular.attack.entities.sql.User;
 import com.abmv.angular.attack.service.BookService;
 import com.abmv.angular.attack.service.UserService;
@@ -29,41 +28,10 @@ public class BookRestService {
 	@Autowired
 	UserService usrSer;
 	
-	@RequestMapping("/addBook")
-	public Book addBook(){
-		Book b=new Book();
-		b.setTitle("Hobbit");
-		b.setAuthorName("Peter Jackson");
-		b.setRating(5);
-		b.setOwner(usrSer.getUserById(1l));
-		Set<Genre> g=new HashSet<>();
-		g.add(Genre.Adeventure);
-		g.add(Genre.Drama);
-		b.setGenres(g);
+	@RequestMapping(value= "/addBook", method = RequestMethod.POST,
+			consumes={MediaType.APPLICATION_JSON_VALUE})
+	public Book addBook(@RequestBody Book b){
 		b=bookSer.addBook(b);
-		
-		b=new Book();
-		b.setTitle("The Dark knight");
-		b.setAuthorName("Chris nolan");
-		b.setRating(5);
-		b.setOwner(usrSer.getUserById(2l));
-		g=new HashSet<>();
-		g.add(Genre.Adeventure);
-		g.add(Genre.ScienceFiction);
-		b.setGenres(g);
-		bookSer.addBook(b);
-
-		b=new Book();
-		b.setTitle("Hobbit");
-		b.setAuthorName("Peter Jackson");
-		b.setRating(5);
-		b.setOwner(usrSer.getUserById(2l));
-		g=new HashSet<>();
-		g.add(Genre.Adeventure);
-		g.add(Genre.Drama);
-		b.setGenres(g);
-		b=bookSer.addBook(b);
-		
 		return b;
 	}
 	
