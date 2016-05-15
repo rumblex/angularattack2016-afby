545deda2705e885ead0a4937e0c79ea46d266dc0
Vikash Kaushik <Vikash Kaushik>
2016-05-16 04:03:25 +0530

    updated code to integrate with app

--- AngularAttack2016_ABMV/src/main/java/com/abmv/angular/attack/social/controller/SigninController.java
+++ AngularAttack2016_ABMV/src/main/java/com/abmv/angular/attack/social/controller/SigninController.java
@@ -1,15 +1,34 @@
 package com.abmv.angular.attack.social.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.NativeWebRequest;
 
 @Controller
 public class SigninController {
+	protected static final Logger log = LoggerFactory.getLogger(SigninController.class);
 
 	@RequestMapping(value="/signin", method=RequestMethod.GET)
 	public void signin() {
 		SecurityContextHolder.getContext().getAuthentication();
+	}
+	
+	@RequestMapping(value="/gotoconnect", method=RequestMethod.GET)
+	public String gotoConnect(Model model, @RequestParam String bookName, NativeWebRequest request, HttpServletRequest httpRequest) {
+		log.info("Received book name: "+ bookName);
+		if(bookName!=null){
+			request.setAttribute("bookName", bookName, NativeWebRequest.SCOPE_GLOBAL_SESSION);
+			httpRequest.getServletContext().setAttribute("bookName", bookName);
+			model.addAttribute("bookName", bookName);
+		}
+		return "redirect:/connect";
 	}
 }
--- AngularAttack2016_ABMV/src/main/java/com/abmv/angular/attack/social/controller/SinglePageConnectController.java
+++ AngularAttack2016_ABMV/src/main/java/com/abmv/angular/attack/social/controller/SinglePageConnectController.java
@@ -1,5 +1,7 @@
 package com.abmv.angular.attack.social.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.web.ConnectController;
@@ -7,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.context.request.NativeWebRequest;
 
 public class SinglePageConnectController  extends ConnectController{
+	protected static final Logger log = LoggerFactory.getLogger(SinglePageConnectController.class);
+	
 	public SinglePageConnectController(ConnectionFactoryLocator connectionFactoryLocator, ConnectionRepository connectionRepository) {
 		super(connectionFactoryLocator, connectionRepository);
 	}
@@ -16,6 +20,8 @@ public class SinglePageConnectController  extends ConnectController{
 	 */
 	@Override
 	public String connectionStatus(String providerId, NativeWebRequest request, Model model) {
+		String bookName = request.getAttribute("bookName", NativeWebRequest.SCOPE_GLOBAL_SESSION)!=null ? (String) request.getAttribute("bookName", NativeWebRequest.SCOPE_GLOBAL_SESSION):null;
+		log.info("ConnectController received book name as "+bookName);
 		return "redirect:/connect";
 	}
 }
